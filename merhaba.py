9382ae8af76e06f1affecc1687bf5ea1f645e3cd
Mete928gith <metektt@gmail.com>
2026-07-10 14:21:54 -0400

    i upgraded merhaba.py file(2)
    i changed some things on merhaba.py
    here is it:

--- merhaba.py
+++ merhaba.py
@@ -3,3 +3,19 @@ if weather == "rainy":
     print("take a umbrella")
 else:
     print("you are good")
+
+x = 0
+
+whihe < 10:
+print(" {} bigger than 10" .format(x))
+x += 1
+else:
+print(" {} is less than 10" .format(x))
+
+words = ["a", "b", "c", "e"]
+for index, word in enumerate(words):
+    if word == "c":
+    print(" {} word {} in index" .format(word, index))
+
+list(range(10))
+print(list(range(10))
